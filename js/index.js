bf467f2ce0381ee675c7e3173dda9ec5d1550808
Robin Verona <contact@robinverona.com>
2023-09-27 11:02:13 +0200

    styles && swiper tweaks

--- js/index.js
+++ js/index.js
@@ -3,7 +3,19 @@ console.log('script loaded');
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     spaceBetween: 30,
     mousewheel: true,
+    mousewheelControl: true, 
+    parallax: true,
+    speed: 800,
+    mousewheelOptions: {
+        sensibility: 0.2,
+        thresholdTime: 1000,
+        thresholdDelta: 1000,
+    },
     loop: true,
   });
